7b3dc84a9b845ef72e184b17baca8ebdf6da83fd
vijayakumarponraj <50840993+vijayakumarponraj@users.noreply.github.com>
2019-06-16 22:26:14 +0530

    Add files via upload

---
+++ Edited/end.js
@@ -0,0 +1,10 @@
+function login(){
+		var Name = $('#name_field').val();
+		var password = $('#ps').val()
+		localStorage.setItem('name_field',Name);
+		/*if(userName && password){
+			window.location.href = "Homealogin.html";
+		}*/
+		
+	}
+  </script>
